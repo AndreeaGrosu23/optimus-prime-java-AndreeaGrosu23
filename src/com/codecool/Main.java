f3d6061418ebe8139f22cbfc4df809929a423dae
Andreea Grosu <andreea.grosu87@gmail.com>
2020-06-07 15:47:15 +0300

    Solution for the exercise

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -3,6 +3,10 @@ package com.codecool;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int n = 300;
+        System.out.print("Following are the prime numbers ");
+        System.out.println("smaller than or equal to " + n);
+        SieveOfEratosthenes g = new SieveOfEratosthenes();
+        g.sieveOfEratosthenes(n);
     }
 }
